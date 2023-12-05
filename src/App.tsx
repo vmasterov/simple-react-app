97ee24514d68cb062d4eeb599a7be5d124c3f1b3
vladimir.masterov <xiii555iiix@yandex.ru>
2023-12-05 07:43:40 +0300

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import 'App.css';
+import 'App.module.css';
 import MainPage from 'pages/MainPage';
 
 function App() {
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useMemo, useState } from 'react';
 import { FactType } from 'types/Fact.type';
 import factsService from 'services/facts.service';
+import styles from 'App.module.css';
 
 export default function MainPage() {
   const [randomFact, setRandomFact] = useState<FactType>({} as FactType);
   const [todayFact, setTodayFact] = useState<FactType>({} as FactType);
-
-  const fact = useMemo(() => (randomFact.text ? randomFact.text : todayFact.text), [todayFact, randomFact]);
+  const fact = useMemo(() => (randomFact.text ? randomFact : todayFact), [todayFact, randomFact]);
 
   const loadRandomFact = async () => {
     const fact = await factsService.getRandomFact();
@@ -23,12 +23,29 @@ export default function MainPage() {
   }, []);
 
   return (
-    <>
-      <h1>The fact</h1>
-      <button type="button" onClick={loadRandomFact}>
-        Get new one
-      </button>
-      {fact && <div>{fact}</div>}
-    </>
+    <div className={styles.FactBlockWrapper}>
+      <div className={styles.FactBlock}>
+        <div className={styles.Head}>
+          <h1>The fact</h1>
+          <button className={styles.NewFactButton} type="button" onClick={loadRandomFact}>
+            New fact
+          </button>
+        </div>
+        {fact.text && (
+          <div className={styles.Body}>
+            <div className={styles.Caption}>Fact:</div>
+            <div className={styles.Text}>{fact.text}</div>
+            <div className={styles.Caption}>Type:</div>
+            <div className={styles.Text}>{fact.type}</div>
+            <div className={styles.Caption}>Source:</div>
+            <div className={styles.Text}>
+              <a href={`https://${fact.source}`} target="_blank" rel="noreferrer">
+                {fact.source}
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
